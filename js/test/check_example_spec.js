52202f8599900c5935bdc1111b90763f3fb54fc6
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2016-06-06 11:57:31 +0100

    Updated initial check to be focused on UI rather than BE change

--- js/test/check_example_spec.js
+++ js/test/check_example_spec.js
@@ -6,8 +6,10 @@ var expect = chai.expect;
 describe('Initial JS unit check', function(){
 
   it('should pull down the site and run assertion', function(done){
-    page.build('/enter_bug.cgi', function(window){
-      expect(window.$('a[href="index.cgi?logout=1"]').length).to.equal(2);
+    page.build('/enter_bug.cgi?product=FoodReplicator', function(window){
+      window.show_comment_preview();
+
+      expect(window.$('.bz_comment.bz_default_hidden').length).to.equal(0);
 
       done();
     });
