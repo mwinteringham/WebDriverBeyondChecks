b85827537bba76af6da932de4416e1b57d5254ab
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2016-06-06 11:15:42 +0100

    Updated authentication for page builder to get credentials from json and make a request for cookies.  Added README.md

---
+++ js/README.md
@@ -0,0 +1,12 @@
+# WebDriverBeyondChecks
+
+## Pre-requisites
+
+* nodejs 5.0.0
+
+## Setup
+
+1. Clone repo to a local machine
+2. Navigate into ```WebDriverBeyondChecks/js```
+3. Run ```npm install```
+4. Once installed run ```npm test``` to run checks
--- js/test/check_example_spec.js
+++ js/test/check_example_spec.js
@@ -1,12 +1,12 @@
 var chai  = require('chai'),
-    dom = require('./dom_builder.js');
+    page = require('./helpers/page_builder.js');
 
 var expect = chai.expect;
 
 describe('Initial JS unit check', function(){
 
   it('should pull down the site and run assertion', function(done){
-    dom.build('/enter_bug.cgi', function(window){
+    page.build('/enter_bug.cgi', function(window){
       expect(window.$('a[href="index.cgi?logout=1"]').length).to.equal(2);
 
       done();
