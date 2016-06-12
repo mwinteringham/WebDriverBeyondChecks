b66f7f72933f049687ab31f07a09f3a04d85e831
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2016-06-12 21:52:26 +0100

    Initial setup of API framework

---
+++ api/src/test/java/com/beyondchecks/api/ApiTest.java
@@ -0,0 +1,14 @@
+package com.beyondchecks.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ApiTest {
+
+    @Test
+    public void initialTest(){
+        assertEquals(1, 1);
+    }
+
+}
