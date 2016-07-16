b0bca8f950170d5a43597796b7452a745f0cc675
Richard Bradshaw <richardbradshaw@gmail.com>
2016-07-17 00:47:19 +0100

    Started WebDriver Project

---
+++ webdriver/src/main/java/pageobjects/InvalidLoginPage.java
@@ -0,0 +1,14 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by richard on 16/07/2016.
+ */
+public class InvalidLoginPage extends PageObject
+{
+    public InvalidLoginPage(WebDriver driver)
+    {
+        super(driver);
+    }
+}
---
+++ webdriver/src/main/java/pageobjects/RequestToChangePasswordPage.java
@@ -0,0 +1,14 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by richard on 17/07/2016.
+ */
+public class RequestToChangePasswordPage extends PageObject
+{
+    public RequestToChangePasswordPage(WebDriver driver)
+    {
+        super(driver);
+    }
+}
