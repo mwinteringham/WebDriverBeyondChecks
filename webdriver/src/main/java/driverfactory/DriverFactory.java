7cff3919a5471be67d4d4adeb4207a652cd85c4c
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2016-08-07 11:41:56 -0700

    Expanded driver factory to use FF

--- webdriver/src/main/java/driverfactory/DriverFactory.java
+++ webdriver/src/main/java/driverfactory/DriverFactory.java
@@ -11,10 +11,23 @@ public class DriverFactory
 {
     public WebDriver create()
     {
+        try{
+            if(System.getenv("browser").equals("firefox")){
+                return new FirefoxDriver();
+            } else {
+                ChromeDriver driver = generateChromeDriver();
+                return driver;
+            }
+        } catch (NullPointerException e){
+            ChromeDriver driver = generateChromeDriver();
+            return driver;
+        }
+
+    }
+
+    private ChromeDriver generateChromeDriver(){
         String pathToChromeDriver = System.getProperty("user.dir") + "/src/main/java/driverfactory/chromedriver";
         System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
-        ChromeDriver driver = new ChromeDriver();
-
-        return driver;
+        return new ChromeDriver();
     }
 }
