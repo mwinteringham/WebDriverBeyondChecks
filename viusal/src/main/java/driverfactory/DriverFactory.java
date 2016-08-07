765234675405d597a179459f222c61d8b57d2e78
Richard Bradshaw <richardbradshaw@gmail.com>
2016-08-07 11:44:11 -0700

    updated second drvier factory

--- viusal/src/main/java/driverfactory/DriverFactory.java
+++ viusal/src/main/java/driverfactory/DriverFactory.java
@@ -2,6 +2,7 @@ package driverfactory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by richard on 17/07/2016.
@@ -10,10 +11,23 @@ public class DriverFactory
 {
     public WebDriver create()
     {
+        try{
+            if(System.getenv("browser").equals("firefox")){
+                return new FirefoxDriver();
+            } else {
+                ChromeDriver driver = generateChromeDriver();
+                return driver;
+            }
+        } catch (NullPointerException e){
+            ChromeDriver driver = generateChromeDriver();
+            return driver;
+        }
+
+    }
+
+    private ChromeDriver generateChromeDriver(){
         String pathToChromeDriver = System.getProperty("user.dir") + "/src/main/java/driverfactory/chromedriver";
         System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
-        ChromeDriver driver = new ChromeDriver();
-
-        return driver;
+        return new ChromeDriver();
     }
 }
